1bb98f746dfc816a5685bb5bbe8550eb55aba1e7
NAVEEN PATIL <naveenpatil@Mac.lan>
2026-02-01 22:03:25 +0530

    addition pgm

---
+++ date1-02-26.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+/*additon program */
+/*int date;
+float date1;
+float date2;
+date1=5.5;
+date2=10.5;
+date=date1+date2;
+scanf("%i",&date);
+printf("todays_date_is=%d\n",(void *)&date);
+printf("todays_date_is=%d\n",date1);
+printf("todays_date_is=%d\n",date2);
+printf("todays_date_is=%i\n",date);
+*/
+
+/*multpication program */
+
+
+int date;
+float date1;
+float date2;
+date1=5.5;
+date2=10.5;
+date=date1+date2;
+scanf("%i",&date);
+printf("todays_date_is=%d\n",date);
+printf("todays_date_is=%f\n",date1);
+printf("todays_date_is=%f\n",date2);
+printf("todays_date_is=%i\n",date);
+
+
+/*divison program */
+
+return 0;
+}
