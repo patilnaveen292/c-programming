ff03fe8aae8033ce10bd05471f3b4815c6f44340
NAVEEN PATIL <naveenpatil@Mac.lan>
2026-02-10 14:26:45 +0530

    todays pgm

---
+++ date10-2-26.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int marks = 98;
+    char gender ='M';
+    float avg = 9.8;
+    double height = 5.7;
+
+    printf("marks = %d\n gender = %c\n average = %f\n height is %lf\n ",marks,gender,avg,height);
+    return 0;
+}
